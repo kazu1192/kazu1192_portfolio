f0082479f40f7856c648af1e90822b618b8f284d
kazu1192 <dye.deep.red@gmail.com>
2019-05-16 20:21:02 +0900

    portfolio completed.

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,6 +1,8 @@
 <template>
   <section class="contact" id="contact">
     <h2 class="heading">CONTACT</h2>
+    <p>Twitter: https://twitter.com/kazu1192_pg</p>
+    <p>Facebook: https://www.facebook.com/kazu1192</p>
     <p>Github: https://github.com/kazu1192</p>
   </section>
 </template>
@@ -10,6 +12,7 @@
 
 <style>
 .contact {
+  font-size: 1.8rem;
   padding: 80px 0 150px;
 }
 </style>
--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,7 +1,26 @@
 <template>
   <section class="skills" id="skills">
     <h2 class="heading">SKILLS</h2>
-    <p class="skills-wrapper">TODO IN SKILLS</p>
+    <p class="skills-text">使用可能な言語スキル</p>
+    <div class="skills-wrapper">
+      <img src="@/assets/images/svg/ruby.svg" alt="Ruby">
+      <img src="@/assets/images/svg/rails.svg" alt="Ruby on Rails">
+      <img src="@/assets/images/svg/java.svg" alt="Java">
+      <img src="@/assets/images/svg/struts.svg" alt="Struts">
+      <img src="@/assets/images/svg/spring.svg" alt="Spring">
+      <img src="@/assets/images/svg/android-icon.svg" alt="Android">
+      <img src="@/assets/images/svg/php.svg" alt="PHP">
+      <img src="@/assets/images/svg/html-5.svg" alt="html">
+      <img src="@/assets/images/svg/css-3.svg" alt="css">
+      <img src="@/assets/images/svg/javascript.svg" alt="JS">
+      <img src="@/assets/images/svg/nodejs.svg" alt="nodejs">
+      <img src="@/assets/images/svg/vue.svg" alt="vuejs">
+      <img src="@/assets/images/svg/mysql.svg" alt="mysql">
+      <img src="@/assets/images/svg/git.svg" alt="git">
+      <img src="@/assets/images/svg/linux-tux.svg" alt="linux">
+      <img src="@/assets/images/svg/centos.svg" alt="centos">
+      <img src="@/assets/images/svg/archlinux.svg" alt="Arch">
+    </div>
   </section>
 </template>
 
@@ -11,6 +30,21 @@
 <style>
 .skills {
   background-color: #383634;
-  padding: 80px 0px;
+  padding: 80px 0;
+}
+.skills-text {
+  font-size: 2.4rem;
+}
+.skills-wrapper {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  width: 80%;
+  margin: 30px auto 0;
+}
+.skills-wrapper img {
+  width: auto;
+  height: 80px;
+  margin: 10px 10px;
 }
 </style>
